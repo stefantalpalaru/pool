a0d2195de6f44f0cbe933178d8154b2eb789a7a8
Stefan Talpalaru <stefantalpalaru@yahoo.com>
2013-02-10 22:03:33 +0100

    initial commit

---
+++ example/pool_example.go
@@ -0,0 +1,68 @@
+/* Copyright (c) 2013, Stefan Talpalaru <stefan.talpalaru@od-eon.com>, Odeon Consulting Group Pte Ltd <od-eon.com>
+ * All rights reserved. */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package main
+
+import (
+	"pool"
+	"runtime"
+	"log"
+	"math"
+)
+
+func worker(args ...interface{}) interface{} {
+	/*panic("test panic")*/
+	x := args[0].(float64)
+	j := 0.
+	for i:=1.0; i<10000000; i++ {
+		j += math.Sqrt(i)
+	}
+	return x * x + j
+}
+
+func main() {
+	cpus := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpus)
+
+	mypool := pool.NewPool(cpus)
+	mypool.Run()
+	num_jobs := float64(1000)
+
+	for i:=float64(0); i<num_jobs; i++ {
+		mypool.Add(worker, i)
+	}
+	mypool.Wait()
+	sum := float64(0)
+	completed_jobs := mypool.Results()
+	for _, job := range(completed_jobs) {
+		if job.Result == nil {
+			log.Println("got error:", job.Err)
+		} else {
+			sum += job.Result.(float64)
+		}
+	}
+	log.Println(sum)
+
+	// use one result at a time as it becomes available
+	for i:=float64(0); i<num_jobs; i++ {
+		mypool.Add(worker, i)
+	}
+	sum = float64(0)
+	for {
+		job := mypool.WaitForJob()
+		if job == nil {
+			break
+		}
+		if job.Result == nil {
+			log.Println("got error:", job.Err)
+		} else {
+			sum += job.Result.(float64)
+		}
+	}
+	log.Println(sum)
+}
+
---
+++ pool.go
@@ -0,0 +1,180 @@
+/* Copyright (c) 2013, Stefan Talpalaru <stefan.talpalaru@od-eon.com>, Odeon Consulting Group Pte Ltd <od-eon.com>
+ * All rights reserved. */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Package pool provides a worker pool.
+package pool
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+// Job holds all the data related to a worker's instance.
+type Job struct {
+	f func(...interface{}) interface{}
+	args []interface{}
+	Result interface{}
+	Err error
+}
+
+// Pool is the main data structure.
+type Pool struct {
+	num_workers int
+	job_pipe chan *Job
+	done_pipe chan *Job
+	add_pipe chan *Job
+	result_pipe chan *Job
+	jobs_ready_to_run []*Job
+	jobs_running int
+	jobs_completed []*Job
+	interval time.Duration // for sleeping, in ms
+	working_pipe chan bool
+}
+
+// subworker catches any panic while running the job.
+func (pool *Pool) subworker(job *Job) {
+	defer func () {
+		if err := recover(); err != nil {
+			log.Println("panic while running job:", err)
+			job.Result = nil
+			job.Err = fmt.Errorf(err.(string))
+		}
+	}()
+	job.Result = job.f(job.args...)
+}
+
+// worker gets a job from the job_pipe, passes it to a
+// subworker and puts the job in the done_pipe when finished.
+func (pool *Pool) worker(num int) {
+	for {
+		job := <-pool.job_pipe
+		pool.subworker(job)
+		pool.done_pipe <-job
+	}
+}
+
+// NewPool creates a new Pool.
+func NewPool(workers int) (pool *Pool) {
+	pool = new(Pool)
+	pool.num_workers = workers
+	pool.job_pipe = make(chan *Job)
+	pool.done_pipe = make(chan *Job)
+	pool.add_pipe = make(chan *Job)
+	pool.result_pipe = make(chan *Job)
+	pool.jobs_ready_to_run = make([]*Job, 0)
+	pool.jobs_completed = make([]*Job, 0)
+	pool.working_pipe = make(chan bool)
+	pool.interval = 1
+	for i:=0; i<workers; i++ {
+		go pool.worker(i)
+	}
+	return
+}
+
+// supervisor feeds jobs to workers and keeps track of them.
+func (pool *Pool) supervisor() {
+	for {
+		select {
+		case job := <-pool.add_pipe:
+			pool.jobs_ready_to_run = append(pool.jobs_ready_to_run, job)
+		default:
+		}
+
+		num_ready_jobs := len(pool.jobs_ready_to_run)
+		if num_ready_jobs > 0 {
+			select {
+			case pool.job_pipe <-pool.jobs_ready_to_run[num_ready_jobs - 1]:
+				pool.jobs_running++
+				pool.jobs_ready_to_run = pool.jobs_ready_to_run[:num_ready_jobs - 1]
+			default:
+			}
+		}
+
+		if pool.jobs_running > 0 {
+			select {
+			case job := <-pool.done_pipe:
+				pool.jobs_running--
+				pool.jobs_completed = append(pool.jobs_completed, job)
+			default:
+			}
+		}
+
+		working := true
+		if len(pool.jobs_ready_to_run) == 0 && pool.jobs_running == 0 {
+			working = false
+		}
+		select {
+		case pool.working_pipe <-working:
+		default:
+		}
+
+		res := (*Job)(nil)
+		if len(pool.jobs_completed) > 0 {
+			res = pool.jobs_completed[0]
+		}
+		select {
+		case pool.result_pipe <-res:
+			if len(pool.jobs_completed) > 0 {
+				pool.jobs_completed = pool.jobs_completed[1:]
+			}
+		default:
+		}
+
+		time.Sleep(pool.interval * time.Millisecond)
+	}
+}
+
+// Run starts the Pool by launching a supervisor goroutine.
+// It's OK to start an empty Pool. The jobs will be fed to the workers as soon
+// as they become available.
+func (pool *Pool) Run() {
+	go pool.supervisor()
+}
+
+// Add creates a Job from the given function and args and
+// adds it to the Pool.
+func (pool *Pool) Add(f func(...interface{}) interface{}, args ...interface{}) {
+	pool.add_pipe <-&Job{f, args, nil, nil}
+}
+
+// Wait blocks until all the jobs in the Pool are done.
+func (pool *Pool) Wait() {
+	for <-pool.working_pipe {
+		time.Sleep(pool.interval * time.Millisecond)
+	}
+}
+
+// Results retrieves the completed jobs.
+func (pool *Pool) Results() (res []*Job) {
+	res = make([]*Job, len(pool.jobs_completed))
+	for i, job := range(pool.jobs_completed) {
+		res[i] = job
+	}
+	pool.jobs_completed = pool.jobs_completed[0:0]
+	return
+}
+
+// WaitForJob blocks until a completed job is available and returns it.
+// If there are no jobs running, it returns nil.
+func (pool *Pool) WaitForJob() *Job {
+	for {
+		working := <-pool.working_pipe
+		r := <-pool.result_pipe
+		if r == (*Job)(nil) {
+			if !working {
+				break
+			}
+		} else {
+			return r
+		}
+	}
+	return nil
+}
+
+// TODO: a Status() method that reads from pool.working_pipe and maybe reports some stats about completed/waiting jobs
+
