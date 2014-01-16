a09d53c6038e57bbaa46880e51b019036948a4e6
Stefan Talpalaru <stefantalpalaru@yahoo.com>
2014-01-16 18:46:09 +0100

    update license

--- pool.go
+++ pool.go
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, Stefan Talpalaru <stefan.talpalaru@od-eon.com>, Odeon Consulting Group Pte Ltd <od-eon.com>
+/* Copyright (c) 2013-2014 - Stefan Talpalaru <stefantalpalaru@yahoo.com>
  * All rights reserved. */
 
 /* This Source Code Form is subject to the terms of the Mozilla Public
